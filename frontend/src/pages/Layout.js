5914c50a06b3f20cfb3b88f59607f8548970993d
Christopher Kyle <ckyle6300@outlook.com>
2022-01-24 08:39:20 -0600

    added chapters to player

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -9,10 +9,23 @@ import { useEffect, useState } from 'react';
 
 const Layout = (props) => {
   const podInfo = useSelector((state) => state.podcastInfo);
-  const { episode, chp, podcast, count } = podInfo;
+  const { episode, podcast, count, chapters } = podInfo;
   const [player, setPlayer] = useState();
 
-  useEffect(() => {
+  useEffect(async () => {
+    const chp = [];
+
+    for (let i = 0; i < chapters.length; i++) {
+      console.log(chapters[i]);
+      if (i === chapters.length - 1) {
+        chp.push({ ...chapters[i], endTime: episode.duration });
+        continue;
+      }
+      const current = chapters[i];
+      const next = chapters[i + 1];
+      chp.push({ ...current, endTime: next.startTime });
+    }
+
     if (count === 10) {
       try {
         const TAudio = {
@@ -21,8 +34,9 @@ const Layout = (props) => {
           title: episode.title,
           artist: podcast.title,
           duration: episode.duration,
+          chapters: chp,
         };
-        player.update(TAudio);
+        await player.update(TAudio);
       } catch (err) {
         console.log(err);
       }
@@ -30,24 +44,30 @@ const Layout = (props) => {
     }
 
     if (count === 5) {
-      setPlayer(
-        new Shikwasa({
-          container: document.getElementById('players'),
-          audio: {
-            src: episode.enclosureUrl,
-            cover: episode.feedImage,
-            title: episode.title,
-            artist: podcast.title,
-            duration: episode.duration,
-          },
-          theme: 'dark',
-          speedOptions: [0.75, 1, 1.25, 1.5, 1.75, 2, 2.25],
-          autoplay: true,
-        })
-      );
+      await Shikwasa.use(Chapter);
+      try {
+        setPlayer(
+          await new Shikwasa({
+            container: document.getElementById('players'),
+            audio: {
+              src: episode.enclosureUrl,
+              cover: episode.feedImage,
+              title: episode.title,
+              artist: podcast.title,
+              duration: episode.duration,
+              chapters: chp,
+            },
+            theme: 'dark',
+            speedOptions: [0.75, 1, 1.25, 1.5, 1.75, 2, 2.25],
+            autoplay: true,
+          })
+        );
+      } catch (error) {
+        console.log(error);
+      }
     }
   }, [episode]);
 
-  return <IonFooter id='players' />;
+  return <IonFooter className='ion-no-margin' id='players' />;
 };
 export default React.memo(Layout);
