7b709f1d5fed5d69465dc90e11c665f0ad55e34c
Christopher Kyle <ckyle6300@outlook.com>
2022-01-25 13:03:24 -0600

    finished episode modal

--- frontend/src/components/Episodes.js
+++ frontend/src/components/Episodes.js
@@ -1,3 +1,4 @@
+import { lazy } from 'react';
 import {
   IonAvatar,
   IonButton,
@@ -17,9 +18,10 @@ import {
 } from '@ionic/react';
 import { playOutline, closeOutline } from 'ionicons/icons';
 import { useEffect, useState } from 'react';
-import parse from 'html-react-parser';
 
-function parseSecondsIntoReadableTime(milliseconds) {
+import EpisodeModal from './EpisodeModal';
+
+export function parseSecondsIntoReadableTime(milliseconds) {
   //Get hours from seconds
   const hours = milliseconds / (60 * 60);
   const absoluteHours = Math.floor(hours);
@@ -40,57 +42,10 @@ function parseSecondsIntoReadableTime(milliseconds) {
 
 const Episodes = ({ epi, buttonHandler, idx, podTitle }) => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(epi.description);
-  // useEffect(async () => {
-  //   fetch(epi.transcriptUrl)
-  //     .then((response) => response.body)
-  //     .then((rb) => {
-  //       const reader = rb.getReader();
-
-  //       return new ReadableStream({
-  //         start(controller) {
-  //           // The following function handles each data chunk
-  //           function push() {
-  //             // "done" is a Boolean and value a "Uint8Array"
-  //             reader.read().then(({ done, value }) => {
-  //               // If there is no more data to read
-  //               if (done) {
-  //                 console.log('done', done);
-  //                 controller.close();
-  //                 return;
-  //               }
-  //               // Get the data and send it to the browser via the controller
-  //               controller.enqueue(value);
-  //               // Check chunks by logging to the console
-  //               console.log(done, value);
-  //               push();
-  //             });
-  //           }
-
-  //           push();
-  //         },
-  //       });
-  //     })
-  //     .then((stream) => {
-  //       // Respond with our stream
-  //       return new Response(stream, {
-  //         headers: { 'Content-Type': 'text/html' },
-  //       }).text();
-  //     })
-  //     .then((result) => {
-  //       // Do things with result
-  //       console.log(result);
-  //     });
-  // }, []);
-
-  const description = new DOMParser().parseFromString(
-    epi.description,
-    'text/xml'
-  );
 
   return (
     <>
-      <IonItem color='dark'>
+      <IonItem color='dark' button={true}>
         <IonAvatar slot='start'>
           <IonImg src={epi.feedImage} />
         </IonAvatar>
@@ -111,52 +66,14 @@ const Episodes = ({ epi, buttonHandler, idx, podTitle }) => {
           onClick={() => buttonHandler(idx)}
         />
       </IonItem>
-      <IonModal isOpen={isOpen}>
-        <IonHeader>
-          <IonToolbar color='primary'>
-            <IonTitle>{epi.title}</IonTitle>
-            <IonButtons slot='end'>
-              <IonButton
-                fill='clear'
-                color='dark'
-                onClick={() => setIsOpen((prev) => !prev)}
-              >
-                close
-                <IonIcon slot='end' icon={closeOutline} />
-              </IonButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent color='secondary'>
-          <IonGrid>
-            <IonRow>
-              <IonCol>
-                <IonItem color='dark'>
-                  <IonAvatar slot='start'>
-                    <IonImg src={epi.feedImage} />
-                  </IonAvatar>
-                  <IonLabel className='ion-text-wrap'>
-                    <p>{podTitle}</p>
-                    <h2>{epi.title}</h2>
-                    <h3>{epi.datePublishedPretty}</h3>
-                    {epi.duration > 0 && (
-                      <p>{parseSecondsIntoReadableTime(epi.duration)}</p>
-                    )}
-                  </IonLabel>
-                  <IonIcon
-                    slot='end'
-                    button
-                    icon={playOutline}
-                    onClick={() => buttonHandler(idx)}
-                  />
-                </IonItem>
-              </IonCol>
-            </IonRow>
-
-            {parse(epi.description)}
-          </IonGrid>
-        </IonContent>
-      </IonModal>
+      <EpisodeModal
+        isOpen={isOpen}
+        epi={epi}
+        podTitle={podTitle}
+        setIsOpen={setIsOpen}
+        idx={idx}
+        buttonHandler={buttonHandler}
+      />
     </>
   );
 };
